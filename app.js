4aa5ba0380fa1ec089e24cc52356fc5e3154a211
Santtu <191040319+s2nttu@users.noreply.github.com>
2026-03-14 15:15:10 +0100

    project setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home</h1>");
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
